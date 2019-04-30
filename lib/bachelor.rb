96e5bf6cdf202ad1afea7d32dccfe4e74840623c
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-30 20:55:45 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,75 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |info|
+    if info[:status] = "Winner"
+      return info[:name.to_s].split(" ")[0]
+    end
+  end
 end
 
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, info|
+    info.each do |title|
+      if title["occupation"] == occupation
+        return title["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, info|
+    info.each do |title|
+      if title["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info|
+    info.each do |title|
+      if title["hometown"] == hometown
+        return title["occupation"]
+      end
+    end
+  end
 end
 
+
+
+
 def get_average_age_for_season(data, season)
-  # code here
+ age_total = 0
+ number_of_contestants = 0
+  data[season].each do |contestants|
+    contestants.each do |key, value|
+      if key == "age"
+        number_of_contestants += 1
+    age_total = age_total + value.to_f
 end
+end
+end
+avg_age = (age_total/number_of_contestants).round
+return avg_age
+end
+
+
+
+  #     contestants.each do |contestant|
+  #       age_array << contestant[:age].to_f
+  #   end
+  #   ages = 0
+  #   age_array.each do |i|
+  #     ages += i
+  #   end
+  #
+  #   ages
+  #   ages_2 = age_array.length
+  #   age_avg = ages / ages_2
+  #   return age_avg
+  # end
